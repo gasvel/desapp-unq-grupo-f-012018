e2f35d1ac947daf5a1e3b9bd04b1985c0e052347
jeretome <jeremias.tome@live.com.ar>
2018-03-25 17:49:36 -0300

    Agregados: Modelo de la clase User Creada clase Score

---
+++ desapp-groupf-backend/src/main/java/model/Score.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Score {
+	String description;
+	Integer score;
+	
+	public Score(String description, Integer score) {
+		this.description = description;
+		this.score = score;
+	}
+}
