2a9bfdf4ae109c518dda3530f9f78d95d9fd2129
Gaston Veliez <gastonveliez95@gmail.com>
2018-03-26 18:31:51 -0300

    Agregado: Reservation

--- desapp-groupf-backend/src/main/java/model/Reservation.java
+++ desapp-groupf-backend/src/main/java/model/Reservation.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Reservation {
+	
+	private String timeOfRent;
+	private User client;
+	
+	public Reservation() {
+		
+	}
+	
+	public Reservation(String time, User userClient) {
+		this.client = userClient;
+		this.timeOfRent = time;
+		
+	}
+
+	public String getTimeOfRent() {
+		return timeOfRent;
+	}
+
+	public void setTimeOfRent(String timeOfRent) {
+		this.timeOfRent = timeOfRent;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+	
+	
+	
 
 }
