e3472b5398d0bcb375192038e2ca92dafc305e01
Gaston Veliez <gastonveliez95@gmail.com>
2018-03-27 09:56:29 -0300

    Cambios en frontend: Home-Routing
    -Implementacion minima de ruteo
    -Agregado componente posts
    -Muestra de posts de prueba

--- desapp-grupo-f-frontend/src/app/app.module.ts
+++ desapp-grupo-f-frontend/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    		{
+    			path:'posts',
+    			component:PostsComponent
+    		},
+    		{
+    			path:'',
+    			redirectTo:'/posts',
+    			pathMatch:'full'
+    		}
+    	])
   ],
   providers: [],
   bootstrap: [AppComponent]
