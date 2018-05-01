80720a821ededcaaa2fc902a283648b0ecfc0537
gaston <gastonveliez95@gmail.com>
2018-05-01 16:17:55 -0300

    Creacion de request para controller de users

--- desapp-groupf-backend/src/main/java/service/UserService.java
+++ desapp-groupf-backend/src/main/java/service/UserService.java
@@ -1,9 +1,22 @@
 package service;
 
+import model.ArgumentsValidator;
 import model.User;
 
 public class UserService extends GenericService<User> {
 	
 	private static final long serialVersionUID = 2131359482422367092L;
+	
+	@Override
+	public void save(User user){
+		ArgumentsValidator.validateUser(user);
+		super.save(user);
+	}
+	
+	@Override
+	public void update(User user){
+		ArgumentsValidator.validateUser(user);
+		super.update(user);
+	}
 
 }
--- desapp-groupf-backend/src/main/java/webservice/UserRest.java
+++ desapp-groupf-backend/src/main/java/webservice/UserRest.java
@@ -2,26 +2,25 @@ package webservice;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import model.User;
-import persistence.UserRepository;
 import service.UserService;
 
 @Path("/users")
 public class UserRest {
 
-    public static final int NUMBER_OF_POST = 10;
+    public static final int NUMBER_OF_USERS = 10;
 
-    /**
-     * IMPORTANTE!! - Acá deben inyectar el servicio de la aplicación y este
-     * servicio REST solo debe hacer un delegate simple.
-     */
+
     private UserService userService;
     
 
@@ -38,6 +37,28 @@ public class UserRest {
    @Produces("application/json")
    public void newUser(@RequestBody User user){
 	   this.userService.save(user);
+   }
+   
+   @PUT
+   @Path("/{id}/update")
+   @Produces("application/json")
+   public void updateUser(@RequestParam("id") long id,@RequestBody User user){
+	   this.userService.update(user);
+   }
+   
+   @DELETE
+   @Path("/delete/{id}")
+   @Produces("application/json")
+   public void deleteUser(@RequestParam("id") long id){
+	   User savedUser = this.userService.getById(id);
+	   this.userService.delete(savedUser);
+   }
+   
+   @GET
+   @Path("/user/{id}")
+   @Produces("application/json")
+   public User getUser(@RequestParam("id") long id){
+	   return this.userService.getById(id);
    }
 
     public void setUserService(final UserService userServ) {
