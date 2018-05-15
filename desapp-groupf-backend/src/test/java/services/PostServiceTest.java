0fff9ed5e5fefffd62b0efc0ea49f08ab25c62fd
Gaston Veliez <gastonveliez95@gmail.com>
2018-05-15 17:52:48 -0300

    correccion de test

--- desapp-groupf-backend/src/test/java/services/PostServiceTest.java
+++ desapp-groupf-backend/src/test/java/services/PostServiceTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import model.Build;
