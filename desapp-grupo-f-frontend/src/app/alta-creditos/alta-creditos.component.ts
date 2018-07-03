8e7283773e941e91389b3becd529e2ef48f9b3f4
jeretome <you@example.com>
2018-07-03 18:21:36 -0300

    correccion alta creditos

--- desapp-grupo-f-frontend/src/app/alta-creditos/alta-creditos.component.ts
+++ desapp-grupo-f-frontend/src/app/alta-creditos/alta-creditos.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { UsersService } from '../services/users.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { UpdateService } from '../services/update.service';
 
 @Component({
   selector: 'app-alta-creditos',
@@ -24,7 +25,12 @@ export class AltaCreditosComponent implements OnInit {
   }
   );
 
-  constructor(private routerServ: Router, private route: ActivatedRoute, private userServ: UsersService, private formBuilder: FormBuilder) { }
+  constructor(private routerServ: Router,
+      private route: ActivatedRoute, private userServ: UsersService,
+      private formBuilder: FormBuilder,
+      private updateService: UpdateService
+
+    ) { }
 
   ngOnInit() {
     this.credits =  this.route.snapshot.paramMap.get('credits');
@@ -34,7 +40,7 @@ export class AltaCreditosComponent implements OnInit {
 
   actionCredits(){
     this.userServ.altaCreditos(this.userId,this.form.controls.credits.value).subscribe(
-      res => {console.log(res);location.reload();this.routerServ.navigate(["/posts"])},
+      res => {console.log(res);this.updateService.setUpdate(true);this.routerServ.navigate(["/posts"])},
       error => console.log(error)
     )
   }
@@ -48,6 +54,6 @@ export class AltaCreditosComponent implements OnInit {
   }
 
 
-  
+
 
 }
